0beeff86867ea3c423ef35e8fe0e3c05f4d00229
gowshikk <49055593+gowshikk@users.noreply.github.com>
2019-06-11 10:39:17 +0530

    Create sieve_of_eratosthenes.cpp

---
+++ sieve_of_eratosthenes.cpp
@@ -0,0 +1,36 @@
+#include<ctype.h>
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	long int start,end;
+	cin>>start;
+	cin>>end;
+	int i,j;
+	bool prime[end+1];
+	memset(prime,true,(end+1)*sizeof(bool));
+	if(start==end||start>end)
+	{
+	    cout<<"-1";
+	}
+	else{
+	for(i=2;i*i<=end;i++)
+	{
+	    if(prime[i]==true)
+	    {
+	        for(j=i*i;j<=end;j+=i)
+	        {
+	            prime[j]=false;
+	        }
+	    }
+	}
+	for(i=start+1;i<end;i++)
+	{
+	    if(prime[i])
+	        cout<<i<<" ";
+	}
+	}
+   return 0;
+}
